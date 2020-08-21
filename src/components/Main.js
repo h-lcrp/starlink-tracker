5d7dff8bc8978d56cedd7ba3af053b246720eddf
He Zhu <zhuhe17@uw.edu>
2020-08-20 23:00:17 -0700

    added list section and styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,54 @@
 import React, {Component} from 'react';
 import SatSetting from './SatSetting';
 import SatelliteList from './SatelliteList';
+import { NEARBY_SATELLITE, STARLINK_CATEGORY, SAT_API_KEY } from "../constants";
+import Axios from 'axios';
 
 class Main extends Component {
+    constructor() {
+        super();
+        this.state = {
+            loadingSatellites: false,
+        }
+    }
+
+    // maybe remove this function in the future
+    showNearbySatellite = (setting) => {
+        this.fetchSatellite(setting);
+    }
+
+    fetchSatellite = (setting) => {
+        const { observerLat, observerLong, observerAlt, radius } = setting;
+        const url = `${NEARBY_SATELLITE}/${observerLat}/${observerLong}/${observerAlt}/${radius}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+        
+        this.setState({
+            loadingSatellites: true,  
+        })
+
+        Axios.get(url)
+            .then(response => {
+                this.setState({
+                    satInfo: response.data,
+                    loadingSatellites: false
+                })
+            })
+            .catch(error => {
+                console.log('err in fetching satellite -> ', error);
+                this.setState({
+                    loadingSatellites: false
+                })
+            })
+    }
+    
     render() {
         return (
             <div className='main'>
                 <div className="left-side">
-                    <SatSetting />
-                    <SatelliteList />
+                    <SatSetting onShow={ this.showNearbySatellite }/>
+                    <SatelliteList 
+                        satInfo={ this.state.satInfo }
+                        loading={this.state.loadingSatellites}
+                    />
                 </div>
 
                 <div className="right-side">
